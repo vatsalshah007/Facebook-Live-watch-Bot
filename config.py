28eb31096f52956be2ee9ad93be3282cadcefdab
Vatsal <vapatwa2004@gmail.com>
2020-06-17 04:17:55 +0530

    Initial Commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-Fb_username = 'vapatwa1998@yahoo.co.in'
-Fb_password = 'Ghostrider0071'
-Fb_auth_secret_code = 'EBMHMYIMSLVKUIAIGDU5YTVARY3GPVRX'
+Fb_username = 'username'
+Fb_password = 'password'
+Fb_auth_secret_code = '32_digit_secret_code'
