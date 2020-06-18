1d933fd05b55a8025c648a3a7e1bdb0eecd16d67
Vatsal <vapatwa2004@gmail.com>
2020-06-19 00:47:17 +0530

    Added live watch functionality

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-Fb_username = 'username'
-Fb_password = 'password'
-Fb_auth_secret_code = '32_digit_secret_code'
+Fb_username = 'Username'
+Fb_password = 'Password'
+Fb_auth_secret_code = '32_Digit_secret_Code'
+
+team_name = input('Enter your team name: ')
--- index.py
+++ index.py
@@ -4,14 +4,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep
-from config import Fb_username, Fb_password, Fb_auth_secret_code
+from config import Fb_username, Fb_password, Fb_auth_secret_code, team_name
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
 class FB_live_bot():
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_experimental_option('prefs',{'profile.default_content_setting_values.notifications':2})
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.set_window_size(1920,1080)
 
     def login(self):
@@ -34,18 +36,43 @@ class FB_live_bot():
             while (self.driver.find_element_by_xpath('//*[@id="checkpointSubmitButton"]')):    
                 self.login_continue_pg()
                 count += 1
-                sleep(5)
-                element_1 = EC.presence_of_element_located((By.CLASS_NAME, '_3ixn'))
-                element_2 = EC.presence_of_element_located((By.CLASS_NAME,'UIPage_LoggedOut'))
-                if (element_1) and not (element_2):
-                    break
         except Exception:
-            print(count)  
-        anywhere_click = self.driver.find_element_by_xpath('//*[@id="facebook"]/body/div[21]/div[1]')  
-        anywhere_click.click()
+            print(count)
     
-    # def live_watch(self):
-     
+    def live_watch(self):
+        search_ip = self.driver.find_element_by_class_name('_1frb')
+        search_ip.click()    
+        search_ip.send_keys('LaLiga')
+        sleep(3)
+        # wait_1 = WebDriverWait(self.driver, 10)
+        # element_1 = wait_1.until(EC.presence_of_element_located((By.ID, '_585_')))
+        search_btn = self.driver.find_element_by_class_name('_585_')    
+        search_btn.click()
+        wait_2 = WebDriverWait(self.driver, 10)
+        element_2 = wait_2.until(EC.element_to_be_clickable((By.LINK_TEXT, 'LaLiga')))
+        la_liga_btn = self.driver.find_element_by_link_text('LaLiga')
+        la_liga_btn.click()
+        wait_3 = WebDriverWait(self.driver, 10)
+        try:
+            element_3 = wait_3.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="u_fetchstream_3_4"]/div[4]/a/span[1]')))
+            videos_btn = self.driver.find_element_by_xpath('//*[@id="u_fetchstream_3_4"]/div[4]/a/span[1]') 
+            videos_btn.click()
+        except Exception:
+            element_3 = wait_3.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="u_fetchstream_4_4"]/div[4]/a/span[1]')))
+            videos_btn = self.driver.find_element_by_xpath('//*[@id="u_fetchstream_4_4"]/div[4]/a/span[1]') 
+            videos_btn.click()
+            # self.driver.find_element_by_class_name('_2j04')
+               
+        wait_4 = WebDriverWait(self.driver, 10)
+        element_4 = wait_4.until(EC.element_to_be_clickable((By.CLASS_NAME, 'du4w35lb')))
+        team_name_videos_list = bot.driver.find_elements_by_xpath("//*[contains(text(), '{}')]".format(team_name))[1:]
+        print(team_name)
+        print(len(team_name_videos_list))
+        if len(team_name_videos_list) <= 0:
+            print("No such team found")
+            self.driver.quit()
+        else:
+            team_name_videos_list[0].click()
 
     def login_continue_pg(self):
         cont_btn_1 = self.driver.find_element_by_xpath('//*[@id="checkpointSubmitButton"]')
@@ -53,3 +80,4 @@ class FB_live_bot():
 
 bot = FB_live_bot()
 bot.login()
+bot.live_watch()
