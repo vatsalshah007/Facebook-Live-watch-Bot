58cb8af1343926c8a0e83e1bf4f276993f4584a6
Vatsal <vapatwa2004@gmail.com>
2021-02-01 04:07:27 +0530

    Update
    Automatic League finder based on team name

---
+++ league.py
@@ -0,0 +1,11 @@
+from config import team_name
+
+La_liga = {
+    'FC Barcelona' : 'FCB, FC Barcelona, Barca',
+    'Real Madrid' : 'Madrid, Real Madrid',
+    'Athletico Madrid' : 'Athletico, Athletico Madrid'
+}
+
+Premiere_League = {
+    'Man U' : 'Manchester United'
+}
